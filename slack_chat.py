7184f02cf89ffa048abeecc5c54ebcac844e8179
Andrea Solinas <analytics1@robin.it>
2025-09-30 16:15:52 +0200

    Create decorator @notify to send message
    @notify send message on Error, it can be suppress raise with 'silent' or 'suppress' boolean argument

--- slack_chat.py
+++ slack_chat.py
@@ -1,8 +1,7 @@
-import logging
-
+from typing import Callable, Any, Optional
+import functools, os, logging, traceback
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-import os
 
 log = logging.getLogger(__name__)
 
@@ -39,3 +38,62 @@ def send(message:str, **kwargs):
     except SlackApiError as e:
         log.exception(f"Error sending message:\n{e.response['error']}")
         return False
+
+
+## DECORATOR
+def notify_on_exception(func: Callable,
+                        *,
+                        silent:Optional[bool]=False,
+                        suppress:Optional[bool]=False,
+                        message:Optional[str]="") -> Callable:
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+
+            tb = traceback.extract_tb(e.__traceback__)
+            if tb:
+                last_frame = tb[-1]
+                filename = last_frame.filename.split('/')[-1]  # Solo il nome del file
+                lineno = last_frame.lineno
+            else:
+                filename = "sconosciuto"
+                lineno = "-1"
+
+            error_msg = f"❌ *Errore* `{type(e).__name__}` [ _app/{filename}_ : {lineno} ] \n\n"
+            error_msg += f"La funzione `{func.__name__}` ha generato un `{type(e).__name__}`:"
+            error_msg += f"\t```{str(e)}\n{message}```\n"
+
+            try:
+                send(error_msg)
+            except Exception as e:
+                log.exception(f"Error sending error message:\n{e}")
+            if not silent and not suppress:
+                raise
+
+    return wrapper
+
+def notify_on_logging(func: Callable,
+                      *,
+                      level: int = logging.ERROR
+                      ) -> Callable:
+    #TODO: Crea la funzione per intercettare e inviare il logg
+    pass
+
+
+def notify(_function:Callable, **kwargs):
+
+    def decorator(function:Callable) -> Callable:
+        original_function = function
+        # if kwargs.pop("on_log", True):
+        #     original_function = notify_on_logging(function, **kwargs)
+
+        original_function = notify_on_exception(original_function, **kwargs)
+        return original_function
+
+    if _function is not None:
+        return decorator(_function)
+
+    return decorator
